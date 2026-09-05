9086ef7d04a469ce52e5da488a70a89d531df33b
Gabriel Oliveira <148448623+Gabriel0liv@users.noreply.github.com>
2026-09-05 10:51:46 +0100

    feat: add native stt package

---
+++ speech_worker/stt/__init__.py
@@ -0,0 +1,8 @@
+from speech_worker.stt.types import NormalizedSegment, NormalizedTranscriptResult, NormalizedWord, SttResolvedConfig
+
+__all__ = [
+    "NormalizedSegment",
+    "NormalizedTranscriptResult",
+    "NormalizedWord",
+    "SttResolvedConfig",
+]
